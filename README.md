6a8c3c1c8f2564157c21cf4d73d394a380a2fbde
buttercrab <jaeyong0201@gmail.com>
2019-11-16 22:45:01 +0900

    Initial commit
    added fetch_data.py

---
+++ README.md
@@ -0,0 +1,47 @@
+# Movie Trend
+
+This is movie audience count predictor for South Korea.
+It fetches real audience data from [kobis](http://www.kobis.or.kr/kobis/business/main/main.do) and naver search data from [naver datalab](https://datalab.naver.com/)
+
+## Use yourself
+
+1. get api key from naver datalab
+1. make `secret/secret.txt` and put naver datalab secret key
+1. change user key from `fetch_data.py`
+1. make `data/conf.json`
+1. run `fetch_data.py`
+
+In terminal:
+
+```shell script
+mkdir -p secret data
+echo "YOUR_SECRET_KEY" >> secret/secret.txt
+echo "{}" >> data/conf.json
+python3 fetch_data.py
+```
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2019 Jaeyong Sung
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ fetch_data.py
@@ -0,0 +1,116 @@
+import json
+import urllib.parse
+from datetime import datetime
+from datetime import timedelta
+from urllib.request import urlopen
+
+import requests
+from bs4 import BeautifulSoup
+
+secret_key = ''
+conf = json.loads(open('data/conf.json').readline())
+
+
+def make_date(d: datetime):
+	return "%04d-%02d-%02d" % (d.year, d.month, d.day)
+
+
+def fetch_api_key():
+	global secret_key
+	if secret_key == '':
+		secret = open('secret/secret.txt')
+		secret_key = secret.readline()
+	return secret_key
+
+
+def save_conf():
+	global conf
+	conf_file = open('data/conf.json', 'w')
+	conf_file.write(json.dumps(conf, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+	fetch_api_key()
+	movies = []
+
+	html = urlopen('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
+	bs_object = BeautifulSoup(html, 'html.parser')
+
+	for item in bs_object.select('.tit3'):
+		movies.append(str(item.find_all('a')[0].text))
+
+	dt = datetime.now()
+
+	cnt = 0
+	for movie in movies:
+		cnt += 1
+		print(str(cnt) + ': downloading movie "' + movie + '"')
+		try:
+			encoded = urllib.parse.quote(movie)
+			html = urlopen('http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do?sMovName=' + encoded)
+			bs_object = BeautifulSoup(html, 'html.parser')
+			code = str(bs_object.select('.tac')[0].find_all('span')[0].text).strip()
+
+			encoded = urllib.parse.quote(movie + ' 개봉일')
+			html = urlopen('https://search.naver.com/search.naver?query=' + encoded)
+			bs_object = BeautifulSoup(html, 'html.parser')
+			date = str(bs_object.select('.property')[0].text).split()[0][:-1].replace('.', '-')
+
+			html = urlopen(
+				'http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieDtlXls.do?sType=box&code=' + code)
+			bs_object = BeautifulSoup(html, 'html.parser')
+
+			flag = False
+			audience_data = []
+			for item in bs_object.select('tbody')[0].find_all('tr'):
+				cur_date = str(item.find_all('td')[0].text)
+				if cur_date == date:
+					flag = True
+
+				if flag:
+					audience_data.append({
+						'time': cur_date,
+						'data': int(str(item.find_all('td')[10].text).replace(',', ''))
+					})
+
+			body = {
+				'startDate': make_date(dt - timedelta(days=101)),
+				'endDate': make_date(dt - timedelta(days=1)),
+				'timeUnit': 'date',
+				'keywordGroups': [
+					{
+						'groupName': movie,
+						'keywords': [
+							movie,
+						]
+					},
+				]
+			}
+
+			headers = {
+				'X-Naver-Client-Id': '1vwchK27lb2hC4W3Cufh',
+				'X-Naver-Client-Secret': secret_key,
+				'Content-Type': 'application/json'
+			}
+
+			res = requests.post('https://openapi.naver.com/v1/datalab/search', data=json.dumps(body), headers=headers)
+			search_data = []
+
+			for i in res.json()['results'][0]['data']:
+				search_data.append({
+					'time': i['period'],
+					'data': i['ratio']
+				})
+		except:
+			cnt -= 1
+			continue
+
+		file = open('data/' + movie + '.json', 'w+')
+		file.write(json.dumps({
+			'audience_data': audience_data,
+			'search_data': search_data
+		}))
+		conf[movie] = date
+		save_conf()
+
+	print('downloaded ' + str(cnt) + ' movie data, ' + str(len(movies) - cnt) + ' failed')
