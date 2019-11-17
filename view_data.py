4231eca691b2bb37ab41c4664e8c574dfbc5646e
buttercrab <jaeyong0201@gmail.com>
2019-11-17 13:05:17 +0900

    added view_data.py
    can view movie data into graph

--- view_data.py
+++ view_data.py
@@ -1,4 +1,36 @@
 import json
 
+import matplotlib.font_manager as fm
+from matplotlib import pyplot as plt
+
+# for korean font
+path = '/Library/Fonts/NanumBarunpenRegular.ttf'
+font = fm.FontProperties(fname=path, size=18)
+
 if __name__ == '__main__':
 	conf = json.loads(open('data/conf.json').readline())
+	for movie in conf:
+		data = json.loads(open('data/' + movie + '.json').readline())
+		audience_data = data['audience_data']
+		search_data = data['search_data']
+
+		x, y = [], []
+		for item in search_data:
+			x.append(item['time'])
+			y.append(item['data'])
+		plt.plot(x, y)
+
+		max_value = 0
+		for item in audience_data:
+			max_value = max(max_value, item['data'])
+
+		x, y = [], []
+		for item in audience_data:
+			x.append(item['time'])
+			y.append(float(item['data']) / max_value * 100)
+		plt.plot(x, y)
+
+		plt.xticks(rotation=90)
+
+		plt.title(movie, fontproperties=font)
+		plt.show()
