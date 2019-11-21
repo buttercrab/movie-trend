c83ed4cf0e3244843cc40f6f7c349a9428ded210
buttercrab <jaeyong0201@gmail.com>
2019-11-22 00:02:33 +0900

    edited

--- fetch_data.py
+++ fetch_data.py
@@ -89,6 +89,7 @@ if __name__ == '__main__':
 						'groupName': movie,
 						'keywords': [
 							movie,
+							movie.replace(':', '').replace('  ', ' ')
 						]
 					},
 				]
--- find_fit.py
+++ find_fit.py
@@ -1,10 +1,16 @@
 import argparse
 import json
+from datetime import datetime, timedelta
 
 import matplotlib.font_manager as fm
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
+
+
+def make_date(d: datetime):
+	return "%04d-%02d-%02d" % (d.year, d.month, d.day)
+
 
 # for korean font
 path = 'font/NanumBarunpenRegular.ttf'
@@ -17,10 +23,9 @@ args = parser.parse_args()
 if __name__ == '__main__':
 	conf = json.loads(open('data/conf.json', 'r').readline())
 	cnt = 0
+	r_squares = []
 	for movie in conf:
 		if args.movie is None or args.movie == movie:
-			cnt += 1
-			print(str(cnt) + ': ' + movie)
 			data = json.loads(open('data/' + movie + '.json').readline())
 			audience_data = data['audience_data']
 			search_data = data['search_data']
@@ -39,9 +44,10 @@ if __name__ == '__main__':
 
 			xdata, ydata = [], []
 			for time in search:
-				if time in audience:
+				new_time = make_date(datetime.strptime(time, '%Y-%m-%d') - timedelta(days=0))
+				if new_time in audience:
 					xdata.append(search[time])
-					ydata.append(audience[time])
+					ydata.append(audience[new_time])
 
 			if len(ydata) <= 1:
 				continue
@@ -51,10 +57,29 @@ if __name__ == '__main__':
 				return a * x + b
 
 
-			prop = curve_fit(f, np.array(xdata), np.array(ydata))
+			xdata = np.array(xdata)
+			ydata = np.array(ydata)
+			prop = curve_fit(f, xdata, ydata)
 			a, b = prop[0][0], prop[0][1]
 
 			x = np.linspace(0, 100, 1000)
+
+			ss_res = np.sum((ydata - f(xdata, a, b)) ** 2)
+			ss_tot = np.sum((ydata - np.mean(ydata)) ** 2)
+			r_squared = 1 - ss_res / ss_tot
+
+			# if r_squared < 0.5:
+			# 	continue
+
+			r_squares.append(r_squared)
+
 			plt.scatter(xdata, ydata)
+			plt.plot(x, f(x, a, b))
+			plt.title(movie, fontproperties=font)
 			plt.savefig('plot/' + movie + '.pdf')
 			plt.clf()
+
+			cnt += 1
+			print(str(cnt) + ': ' + movie + '  ' + str(r_squared))
+
+	print('avg: ' + str(np.mean(np.array(r_squares))))
